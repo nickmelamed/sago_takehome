a32c2abf9cdf3c44a939b695072406a6a7945ec0
Nick Melamed <nickmelamed@berkeley.edu>
2025-11-25 15:42:40 -0800

    adding the scoring of a signal

---
+++ src/signal_engine/signal_engine.py
@@ -0,0 +1,22 @@
+''' 
+'''
+
+import datetime
+
+class SignalEngine:
+    WEIGHTS = { # can customize weights depending on how much you believe each category matters 
+        "funding_round": 0.9,
+        "hiring": 0.7,
+        "product_launch": 0.85
+    }
+
+    def score_signal(self, signal):
+        base = self.WEIGHTS.get(signal["type"], 0.3)
+
+        # Freshness boost (how recent is a given signal?) 
+        days_ago = (datetime.datetime.now().date() - 
+                    datetime.datetime.fromisoformat(signal["date"]).date()).days
+        
+        freshness = max(0.1, 1 - min(days_ago / 120, 1))
+
+        return round(base * freshness, 2)
